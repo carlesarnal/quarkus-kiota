f8714e3dccba89a29b9efd4c5ef5b7acdf5106c2
Carles Arnal <carlesarnal92@gmail.com>
2023-11-23 11:44:47 +0100

    Add vertx auth test

--- integration-tests/auth/src/test/java/com/github/andreatp/quarkus/kiota/it/QuarkusKiotaResourceTest.java
+++ integration-tests/auth/src/test/java/com/github/andreatp/quarkus/kiota/it/QuarkusKiotaResourceTest.java
@@ -1,9 +1,5 @@
 package com.github.andreatp.quarkus.kiota.it;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.microsoft.kiota.RequestAdapter;
 import com.microsoft.kiota.authentication.AnonymousAuthenticationProvider;
 import com.microsoft.kiota.http.VertXRequestAdapter;
@@ -13,14 +9,30 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.oidc.server.OidcWiremockTestResource;
 import io.vertx.core.Vertx;
-import io.vertx.ext.auth.authentication.TokenCredentials;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.auth.oauth2.OAuth2Options;
+import io.vertx.ext.auth.oauth2.Oauth2Credentials;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.OAuth2WebClient;
 import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 @QuarkusTestResource(OidcWiremockTestResource.class)
 public class QuarkusKiotaResourceTest {
+
+    @ConfigProperty(name = "quarkus.oidc.auth-server-url")
+    String keycloakUrl;
 
     @Test
     public void testHelloEndpoint() {
@@ -43,5 +55,38 @@ public class QuarkusKiotaResourceTest {
 
         // Assert
         assertEquals("Hello quarkus-kiota", result.getValue());
+    }
+
+    @Test
+    public void testVertxExample() throws Exception {
+        // Arrange
+        Vertx vertx = Vertx.vertx();
+        WebClient webClient = WebClient.create(vertx); //Don't do this in a regular application, the vertx instance must be injected
+
+        OAuth2Options options = new OAuth2Options()
+                .setFlow(OAuth2FlowType.CLIENT)
+                .setClientId(UUID.randomUUID().toString())
+                .setTokenPath(keycloakUrl + "token")
+                .setClientSecret(UUID.randomUUID().toString());
+
+        //Adding random as the mock does not care about concrete values
+        OAuth2Auth oAuth2Auth = OAuth2Auth.create(vertx, options);
+
+        Oauth2Credentials oauth2Credentials = new Oauth2Credentials();
+
+        OAuth2WebClient oAuth2WebClient = OAuth2WebClient.create(webClient, oAuth2Auth)
+                .withCredentials(oauth2Credentials);
+
+        HttpRequest<Greeting> greetingHttpRequest = oAuth2WebClient
+                .get(8081, "localhost", "/quarkus-kiota")
+                .as(BodyCodec.json(Greeting.class));
+
+        CompletableFuture<Greeting> greetingCompletableFuture = new CompletableFuture<>();
+
+        greetingHttpRequest.send().onSuccess(successResult -> {
+            greetingCompletableFuture.complete(successResult.body());
+        });
+
+        assertEquals("Hello quarkus-kiota", greetingCompletableFuture.get().getValue());
     }
 }
